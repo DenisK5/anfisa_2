e9311b927e3d8278d15e7e5c13e9ef04d8dc0be0
DenisK5 <denis.kondrashov1@gmail.com>
2024-03-06 13:04:56 +0300

    Second commit

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 from core.models import PublishedModel
 
+
 class Category(PublishedModel):
     title = models.CharField('Название', max_length=256)
     slug = models.SlugField('Слаг', max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField('Порядок отображения', default=100)
+    output_order = models.PositiveSmallIntegerField('Порядок отображения',
+                                                    default=100)
     
     class Meta:
         verbose_name = 'категория'
@@ -47,22 +49,21 @@ class IceCream(PublishedModel):
     )
     price = models.DecimalField(max_digits=5, decimal_places=2) 
 
-
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
-        verbose_name = 'Обёртка'
+        verbose_name='Обёртка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
-        verbose_name = 'Категория'
+        verbose_name='Категория'
     )
-    toppings = models.ManyToManyField(Topping, verbose_name = 'Топпинги')
+    toppings = models.ManyToManyField(Topping, verbose_name='Топпинги')
     is_on_main = models.BooleanField('На главную', default=False)
     
     class Meta:
